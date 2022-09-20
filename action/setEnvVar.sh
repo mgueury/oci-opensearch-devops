f47f24533d4801715d8050a6ef6b226003529e36
Oracle Public Cloud User <opc@dev2.sub08301607040.mgsharedvcn.oraclevcn.com>
2022-09-20 09:40:35 +0000

    v162

--- action/setEnvVar.sh
+++ action/setEnvVar.sh
@@ -1,4 +1,3 @@
-
 # Namespace
 export TF_VAR_namespace=`oci os ns get | jq -r .data`
 export TF_VAR_tenancy_ocid=`oci iam compartment list --all --compartment-id-in-subtree true --access-level ACCESSIBLE --include-root --raw-output --query "data[?contains(\"id\",'tenancy')].id | [0]"`
@@ -9,7 +8,7 @@ echo TF_VAR_tenancy_ocid=$TF_VAR_tenancy_ocid
 echo TF_VAR_compartment_ocid=$TF_VAR_compartment_ocid
 echo TF_VAR_namespace=$TF_VAR_namespace
 
-# parameter
+# Parameter
 echo TF_VAR_region=$TF_VAR_region
 
 # Vault
